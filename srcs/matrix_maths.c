3e25a7cd3a3df147bc8e848594feb86b3094e178
Debbie Elliott <deelliot@c1r5p7.hive.fi>
2022-10-11 16:58:07 +0300

    removed inversion functions to new file

--- srcs/matrix_maths.c
+++ srcs/matrix_maths.c
@@ -6,7 +6,7 @@
 /*   By: deelliot <deelliot@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 17:21:03 by deelliot          #+#    #+#             */
-/*   Updated: 2022/10/11 11:21:31 by deelliot         ###   ########.fr       */
+/*   Updated: 2022/10/11 16:55:30 by deelliot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,14 +79,11 @@ double	**matrix_multi_square(double **a, double **b, int size)
 
 double	*matrix_tuple_multi(double **a, t_tuple tuple)
 {
-	double **b;
-	int y; //row
+	int y;
 	int i;
 	double res;
 	double *temp;
 
-	b = tuple_to_array(tuple);
-	ft_print_num_array(b, 4, 1);
 	temp = ft_memalloc(4);
 	if (temp)
 	{
@@ -97,14 +94,13 @@ double	*matrix_tuple_multi(double **a, t_tuple tuple)
 			i = 0;
 			while (i < 4)
 			{
-				res = res + a[y][i] * b[i][0];
+				res = res + a[y][i] * tuple.array[i];
 				i++;
 			}
 			temp[y] = res;
 			y++;
 		}
 	}
-	ft_memdelarray((void *)b, 4);
 	return (temp);
 }
 
@@ -149,71 +145,7 @@ double **identity_matrix(void)
 	return (id_matrix);
 }
 
-/* the following functions are needed to invert a matrix.*/
 
-/* determinant of 2 x 2 matrix*/
-
-double	matrix_determinant(double **a)
-{
-	double temp;
-
-	temp = (a[0][0] * a[1][1]) - (a[0][1] * a[1][0]);
-	return (temp);
-}
-
-/* function to remove a specified row and col from a matrix.
-Returns a new matrix with 1 column and 1 row less*/
-
-double	**submatrix(double **a, int size, int row, int col)
-{
-	double **new_matrix;
-	int new_matrix_row;
-	int new_matrix_col;
-	int row_a;
-	int col_a;
-
-	new_matrix = (double **)ft_memallocarray(size - 1, size - 1);
-	row_a = 0;
-	new_matrix_row = 0;
-	while (row_a <= size - 1)
-	{
-		if (row_a == row)
-		{
-			row_a++;
-			continue ;
-		}
-		col_a = 0;
-		new_matrix_col = 0;
-		while (col_a <= size - 1)
-		{
-			if (col_a == col)
-			{
-				col_a++;
-				continue ;
-			}
-			new_matrix[new_matrix_row][new_matrix_col] = a[row_a][col_a];
-			col_a++;
-			new_matrix_col++;
-		}
-		row_a++;
-		new_matrix_row++;
-	}
-	return (new_matrix);
-}
-
-double matrix_minors(double **a, int row, int col)
-{
-	double **temp;
-	double minor;
-
-	temp = submatrix(a, 3, row, col);
-	minor = matrix_determinant(temp);
-
-	free (temp);
-	return (minor);
-
-
-}
 
 /* test function to print arrays*/
 
