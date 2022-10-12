ca1cd450da64e0b2cfece71adec70a04a499b263
Debbie Elliott <rigbydebbie@icloud.com>
2022-10-12 15:07:10 +0300

    added test to main - not sure if you're done with the unit tests, so just change to actual main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: deelliot <deelliot@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 16:02:35 by deelliot          #+#    #+#             */
-/*   Updated: 2022/10/11 17:04:31 by deelliot         ###   ########.fr       */
+/*   Updated: 2022/10/12 14:28:34 by deelliot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,89 +22,117 @@
 // 	return (0);
 // }
 
-void	test_three_matrix_a3(void)
+// void	test_three_matrix_a3(void)
+// {
+// 	double **a3;
+
+// 	a3 = (double **)ft_memallocarray(3, 3);
+
+// 	a3[0][0] = 1;
+// 	a3[0][1] = 2;
+// 	a3[0][2] = 6;
+
+// 	a3[1][0] = -5;
+// 	a3[1][1] = 8;
+// 	a3[1][2] =-4;
+
+// 	a3[2][0] = 2;
+// 	a3[2][1] = 6;
+// 	a3[2][2] = 4;
+
+// 	double minor;
+// 	double cofactor;
+// 	double determinant;
+
+// 	cofactor = matrix_cofactor(a3, 0, 0);
+// 	printf("cofactor = %f\n", cofactor);
+// 	cofactor = matrix_cofactor(a3, 0, 1);
+// 	printf("cofactor = %f\n", cofactor);
+// 	cofactor = matrix_cofactor(a3, 0, 2);
+// 	printf("cofactor = %f\n", cofactor);
+// 	minor = matrix_minors(a3, 0, 0);
+// 	printf("minor: %f\n", minor);
+// }
+
+// void	test_matrix_tuple_multi_four_matrix(void)
+// {
+// 	double **a4;
+
+// 	a4 = (double **)ft_memallocarray(4, 4);
+
+// 	a4[0][0] = -6;
+// 	a4[0][1] = 1;
+// 	a4[0][2] = 1;
+// 	a4[0][3] = 6;
+
+// 	a4[1][0] = -8;
+// 	a4[1][1] = 5;
+// 	a4[1][2] =8;
+// 	a4[1][3] = 6;
+
+// 	a4[2][0] = -1;
+// 	a4[2][1] = 0;
+// 	a4[2][2] = 8;
+// 	a4[2][3] = 2;
+
+// 	a4[3][0] = -7;
+// 	a4[3][1] = 1;
+// 	a4[3][2] = -1;
+// 	a4[3][3] = 1;
+// }
+
+// void	test_matrix_determinant(void)
+// {
+
+// }
+
+// void	test_tuple_reading(void)
+// {
+// 	t_tuple tuple;
+
+// 	tuple.tuple = (t_units){ 1, 3, 5, 0 };
+
+// 	int i;
+// 	i = 0;
+// 	while (i < 4)
+// 	{
+// 		printf("%0.f, ", tuple.array[i]);
+// 		i++;
+// 	}
+// }
+
+// int main(void)
+// {
+// 	// int count;
+// 	// printf("no. cols = %d\n", count);
+
+// 	return (0);
+// }
+
+double *translate(t_tuple tuple, t_fl x, t_fl y, t_fl z)
 {
-	double **a3;
+	t_fl **temp;
+	double *a;
 
-	a3 = (double **)ft_memallocarray(3, 3);
+	a = NULL;
+	temp = identity_matrix();
+	temp[0][3] = x;
+	temp[1][3] = y;
+	temp[2][3] = z;
 
-	a3[0][0] = 1;
-	a3[0][1] = 2;
-	a3[0][2] = 6;
-
-	a3[1][0] = -5;
-	a3[1][1] = 8;
-	a3[1][2] =-4;
-
-	a3[2][0] = 2;
-	a3[2][1] = 6;
-	a3[2][2] = 4;
-
-	double minor;
-	double cofactor;
-	double determinant;
-
-	cofactor = matrix_cofactor(a3, 0, 0);
-	printf("cofactor = %f\n", cofactor);
-	cofactor = matrix_cofactor(a3, 0, 1);
-	printf("cofactor = %f\n", cofactor);
-	cofactor = matrix_cofactor(a3, 0, 2);
-	printf("cofactor = %f\n", cofactor);
-	minor = matrix_minors(a3, 0, 0);
-	printf("minor: %f\n", minor);
-}
-
-void	test_matrix_tuple_multi_four_matrix(void)
-{
-	double **a4;
-
-	a4 = (double **)ft_memallocarray(4, 4);
-
-	a4[0][0] = -6;
-	a4[0][1] = 1;
-	a4[0][2] = 1;
-	a4[0][3] = 6;
-
-	a4[1][0] = -8;
-	a4[1][1] = 5;
-	a4[1][2] =8;
-	a4[1][3] = 6;
-
-	a4[2][0] = -1;
-	a4[2][1] = 0;
-	a4[2][2] = 8;
-	a4[2][3] = 2;
-
-	a4[3][0] = -7;
-	a4[3][1] = 1;
-	a4[3][2] = -1;
-	a4[3][3] = 1;
-}
-
-void	test_matrix_determinant(void)
-{
-
-}
-
-void	test_tuple_reading(void)
-{
-	t_tuple tuple;
-
-	tuple.tuple = (t_units){ 1, 3, 5, 0 };
-
-	int i;
-	i = 0;
-	while (i < 4)
-	{
-		printf("%0.f, ", tuple.array[i]);
-		i++;
-	}
+	a = matrix_tuple_multi(temp, tuple);
+	ft_memdelarray((void *)temp, 4);
+	return (a);
 }
 
 int main(void)
 {
-	// int count;
-	// printf("no. cols = %d\n", count);
+	t_tuple tuple;
+	double *a;
+
+	tuple.tuple = (t_units){ -3.0, 4.0, 5.0, 1.0 };
+	a = translate(tuple, 1, 2, 3);
+	printf("\nfirst double of a = %0.f\n", a[0]);
 
 	return (0);
 }
