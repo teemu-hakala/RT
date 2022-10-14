115d49467aba7b4df521a868c45643b73c3e73e0
Debbie Elliott <deelliot@c1r5p7.hive.fi>
2022-10-14 11:30:59 +0300

    added the sphere_intersect function, and initialisation of objects

--- srcs/intersect.c
+++ srcs/intersect.c
@@ -6,7 +6,7 @@
 /*   By: deelliot <deelliot@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 16:14:00 by deelliot          #+#    #+#             */
-/*   Updated: 2022/10/13 17:45:45 by deelliot         ###   ########.fr       */
+/*   Updated: 2022/10/14 11:27:45 by deelliot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,50 +15,97 @@
 /* circumference of sphere: 2πr.
 Assume for now all spheres are unit spheres, therefore radius of 1 */
 
-void	intersection(t_ray ray, t_objects shape, t_instance instance)
+void	sphere_intersection(t_ray *ray, t_sphere *sphere, t_intersections *array)
 {
+	t_fl discriminant;
+	t_fl a;
+	t_fl b;
+	t_fl c;
+	t_tuple sphere_to_ray;
 
+	sphere_to_ray = tuple_sub(&ray->origin, &sphere->origin);
+	a = dot_product(&ray->direction, &ray->direction);
+	b = 2 * dot_product(&ray->direction, &sphere_to_ray);
+	c = dot_product(&sphere_to_ray, &sphere_to_ray) - 1;
 
-
+	discriminant = (b * b) - 4 * a * c;
+	if (discriminant < 0.0)
+	{
+		array->num = 0;
+		return ;
+	}
+	array->num = 2; // for other shapes can do num = (uint64)discriminant;
+	array->intersections = (t_intersect *)malloc(sizeof(t_intersect) * 2);
+	if (!array->intersections)
+		handle_errors("intersect malloc fail");
+	array->intersections[0].time = (-b - sqrt(discriminant)) / (2 * a);
+	// array->intersections[0].shape = ??  how do we link the sphere here
+	array->intersections[1].time = (-b + sqrt(discriminant)) / (2 * a);
+	// array->intersections[0].shape =
 }
 
-t_object sphere(t_tuple *origin, t_transform transform, t_tuple *colour)
+t_object sphere(t_tuple *origin, t_transform *transform, t_tuple *colour)
 {
 	return ((t_object)
 	{
 		.object.sphere = (t_sphere)
 		{
-			.origin = (t_tuple){origin->tuple.x,
-				origin->tuple.y,
-				origin->tuple.z,
-				origin->tuple.w},
-			.transform = (t_transform){transform.translation,
-				transform.rotation,
-				transform.scale},
-			.colour = (t_argb){origin->tuple.x,
-				origin->tuple.y,
-				origin->tuple.z,
-				origin->tuple.w}
+			.origin = (t_tuple){origin->tuple.units.x,
+				origin->tuple.units.y,
+				origin->tuple.units.z,
+				origin->tuple.units.w},
+			.transform = (t_transform){transform->translation,
+				transform->rotation,
+				transform->scale},
+			.colour = (t_tuple){colour->tuple.colour.a,
+				colour->tuple.colour.r,
+				colour->tuple.colour.g,
+				colour->tuple.colour.b}
 		},
 		.type = OBJECT_SPHERE
 	});
 }
 
-t_argb	hex_to_argb(uint32_t colour)
+t_tuple	hex_to_argb(uint32_t colour)
 {
-	return ((t_argb){.a = ((colour >> 24) & 0xFFu) / (t_fl)0xFFu,
-		.r = ((colour >> 16) & 0xFFu) / (t_fl)0xFFu,
-		.g = ((colour >> 8) & 0xFFu) / (t_fl)0xFFu,
-		.b = (colour & 0xFFu) / (t_fl)0xFFu});
+	return ((t_tuple){.tuple.colour.a = ((colour >> 24) & 0xFFu) / (t_fl)0xFFu,
+		.tuple.colour.r = ((colour >> 16) & 0xFFu) / (t_fl)0xFFu,
+		.tuple.colour.g = ((colour >> 8) & 0xFFu) / (t_fl)0xFFu,
+		.tuple.colour.b = (colour & 0xFFu) / (t_fl)0xFFu});
 }
 
 int main(void)
 {
 	t_ray		ray;
 	t_objects	objects;
-	t_argb		purple;
+	t_tuple		purple;
+	t_transform transform;
+	t_object	object_sphere;
 
 	purple = hex_to_argb(0x4c00b0);
-	ray.origin.tuple = (t_units){ 0.0, 0.0, -5.0, 1.0 };
-	ray.direction.tuple = (t_units){ 0.0, 0.0, 1.0, 0.0 };
+	ray.origin.tuple.units = (t_units){ 0.0, 0.0, -5.0, 1.0 };
+	ray.direction.tuple.units = (t_units){ 0.0, 0.0, 1.0, 0.0 };
+	transform =(t_transform)
+	{
+		.translation = (t_tuple)
+		{
+			.tuple.units = (t_units){ 0.0, 0.0, 0.0, POINT_1 }
+		},
+		.rotation = (t_tuple)
+		{
+			.tuple.units = (t_units){ 0.0, 0.0, 0.0, POINT_1 }
+		},
+		.scale = (t_tuple)
+		{
+			.tuple.units = (t_units){ 1.0, 1.0, 1.0, POINT_1 }
+		}
+	};
+	objects.list = (t_object *)malloc(sizeof(t_object) * 3);
+	if (objects.list == NULL)
+		exit(EXIT_FAILURE);
+	object_sphere = sphere(
+			&(t_tuple){.tuple.units = (t_units){ 0.0, 0.0, 0.0, POINT_1}},
+			&transform,
+			&purple
+		);
 }
