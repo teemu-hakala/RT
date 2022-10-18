bd42e7034fb5f2c8973ae90f91585adfdd7ef8be
Debbie Elliott <rigbydebbie@icloud.com>
2022-10-18 12:56:43 +0300

    made some functions static, removed definition in header file

--- srcs/matrix_inversion.c
+++ srcs/matrix_inversion.c
@@ -6,7 +6,7 @@
 /*   By: deelliot <deelliot@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 11:50:49 by deelliot          #+#    #+#             */
-/*   Updated: 2022/10/18 12:42:23 by deelliot         ###   ########.fr       */
+/*   Updated: 2022/10/18 12:54:32 by deelliot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 /* function to remove a specified row and col from a matrix.
 Returns a new matrix with 1 column and 1 row less*/
 
-t_mtx submatrix(t_mtx *mtx, uint32_t size, uint32_t row, uint32_t col)
+static t_mtx submatrix(t_mtx *mtx, uint32_t size, uint32_t row, uint32_t col)
 {
 	t_mtx		new_matrix;
 	uint32_t	row_new;
@@ -43,7 +43,8 @@ t_mtx submatrix(t_mtx *mtx, uint32_t size, uint32_t row, uint32_t col)
 				col_old++;
 				continue ;
 			}
-			new_matrix.array[(size - 1) * row_new + col_new] = mtx->array[size * row_old + col_old];
+			new_matrix.array[(size - 1) * row_new + col_new] = \
+			mtx->array[size * row_old + col_old];
 			col_old++;
 			col_new++;
 		}
@@ -53,7 +54,7 @@ t_mtx submatrix(t_mtx *mtx, uint32_t size, uint32_t row, uint32_t col)
 	return (new_matrix);
 }
 
-t_fl	matrix_minors(t_mtx *mtx, uint32_t row, uint32_t col, uint32_t size)
+static t_fl	matrix_minors(t_mtx *mtx, uint32_t row, uint32_t col, uint32_t size)
 {
 	t_mtx	temp;
 	t_fl	minor;
@@ -64,7 +65,7 @@ t_fl	matrix_minors(t_mtx *mtx, uint32_t row, uint32_t col, uint32_t size)
 	return (minor);
 }
 
-t_fl	matrix_cofactor(t_mtx *mtx, uint32_t row, uint32_t col, uint32_t size)
+static t_fl	matrix_cofactor(t_mtx *mtx, uint32_t row, uint32_t col, uint32_t size)
 {
 	t_fl	cofactor;
 	t_fl	minor;
@@ -87,12 +88,14 @@ t_fl	determinant(t_mtx *mtx, uint32_t size)
 	col = 0;
 	det = 0;
 	if (size == 2)
-		det = (mtx->array[0] * mtx->array[2 * 1 + 1]) - (mtx->array[2 * 0 + 1] * mtx->array[2 * 1 + 0]);
+		det = (mtx->array[0] * mtx->array[2 * 1 + 1]) - \
+		(mtx->array[2 * 0 + 1] * mtx->array[2 * 1 + 0]);
 	else
 	{
 		while (col < size)
 		{
-			det = det + mtx->array[size * 0 + col] * matrix_cofactor(mtx, 0, col, size);
+			det = det + mtx->array[size * 0 + col] * \
+			matrix_cofactor(mtx, 0, col, size);
 			col++;
 		}
 	}
@@ -114,12 +117,8 @@ void	matrix_inversion(t_mtx_4 *mtx, uint32_t size)
 		printf("det = %f\n", det);
 		return ;
 	}
-
 	else
 	{
-		//temp = (t_fl **)ft_memallocarray(sizeof(t_fl) * size, sizeof(t_fl) * size);
-		//if (temp)
-		//{
 		y = (uint32_t)(-1);
 		while (++y < size)
 		{
@@ -128,7 +127,5 @@ void	matrix_inversion(t_mtx_4 *mtx, uint32_t size)
 				temp.array[size * y + x] = matrix_cofactor((t_mtx *)mtx, y, x, size) / det;
 		}
 		*mtx = transpose_matrix(&temp);
-		//}
-		//ft_memdelarray((void **)temp, size);
 	}
 }
