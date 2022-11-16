bffd02c0a319fc313b68833bba66018b83ddb76b
Debbie Elliott <rigbydebbie@icloud.com>
2022-11-16 15:54:32 +0200

    fixed cone and sphere normal at functions

--- srcs/normals.c
+++ srcs/normals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   normals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: deelliot <deelliot@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 10:10:20 by deelliot          #+#    #+#             */
-/*   Updated: 2022/11/16 15:14:49 by thakala          ###   ########.fr       */
+/*   Updated: 2022/11/16 15:52:34 by deelliot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,19 +58,21 @@ t_tuple	normal_at_cone(void *cone, t_tuple *point_at)
 {
 	t_fl	distance;
 	t_fl	y;
+	t_tuple	obj_pt;
 
-	y = sqrt((point_at->tuple.units.x * point_at->tuple.units.x) + \
-		(point_at->tuple.units.z * point_at->tuple.units.z));
-	if (point_at->tuple.units.y > 0)
+	obj_pt = matrix_tuple_multi(&((t_cone *)cone)->transform.inverse, point_at);
+	y = sqrt((obj_pt.tuple.units.x * obj_pt.tuple.units.x) + \
+		(obj_pt.tuple.units.z * obj_pt.tuple.units.z));
+	if (obj_pt.tuple.units.y > 0)
 		y *= -1;
-	distance = (point_at->tuple.units.x * point_at->tuple.units.x) + \
-		(point_at->tuple.units.z * point_at->tuple.units.z);
-	if (distance < 1 && (point_at->tuple.units.y >= \
+	distance = (obj_pt.tuple.units.x * obj_pt.tuple.units.x) + \
+		(obj_pt.tuple.units.z * obj_pt.tuple.units.z);
+	if (distance < 1 && (obj_pt.tuple.units.y >= \
 		((((t_object *)cone)->object.cone.max) - EPSILON)))
 		return (normalize(object_to_world_space(\
 			&((t_object *)cone)->object.cone.transform.inverse, \
 			vector(0, 1, 0))));
-	else if (distance < 1 && (point_at->tuple.units.y <= \
+	else if (distance < 1 && (obj_pt.tuple.units.y <= \
 		((((t_object *)cone)->object.cone.min) + EPSILON)))
 		return ((normalize(object_to_world_space(\
 			&((t_object *)cone)->object.cone.transform.inverse, \
@@ -78,21 +80,25 @@ t_tuple	normal_at_cone(void *cone, t_tuple *point_at)
 	else
 		return (normalize(object_to_world_space(\
 			&((t_object *)cone)->object.cone.transform.inverse, \
-			vector(point_at->tuple.units.x, y, point_at->tuple.units.z))));
+			vector(obj_pt.tuple.units.x, y, obj_pt.tuple.units.z))));
 }
 
 t_tuple	normal_at_cylinder(void *cylinder, t_tuple *point_at)
 {
 	t_fl	distance;
+	t_tuple	obj_point;
 
-	distance = (point_at->tuple.units.x * point_at->tuple.units.x) + \
-		(point_at->tuple.units.z * point_at->tuple.units.z);
-	if (distance < 1 && (point_at->tuple.units.y >= \
+	obj_point = matrix_tuple_multi(&((t_cylinder *)cylinder)->transform.inverse,
+			point_at);
+
+	distance = (obj_point.tuple.units.x * obj_point.tuple.units.x) + \
+		(obj_point.tuple.units.z * obj_point.tuple.units.z);
+	if (distance < 1 && (obj_point.tuple.units.y >= \
 		((((t_object *)cylinder)->object.cylinder.max) - EPSILON)))
 		return (normalize(object_to_world_space(\
 			&((t_object *)cylinder)->object.cylinder.transform.inverse, \
 			vector(0, 1, 0))));
-	else if (distance < 1 && (point_at->tuple.units.y <= \
+	else if (distance < 1 && (obj_point.tuple.units.y <= \
 		((((t_object *)cylinder)->object.cylinder.min) + EPSILON)))
 		return (normalize(object_to_world_space(\
 			&((t_object *)cylinder)->object.cylinder.transform.inverse, \
@@ -100,7 +106,7 @@ t_tuple	normal_at_cylinder(void *cylinder, t_tuple *point_at)
 	else
 		return (normalize(object_to_world_space(\
 			&((t_object *)cylinder)->object.cylinder.transform.inverse, \
-			vector(point_at->tuple.units.x, 0, point_at->tuple.units.z))));
+			vector(obj_point.tuple.units.x, 0, obj_point.tuple.units.z))));
 }
 
 t_tuple	normal_at(void *object, t_tuple *point)
