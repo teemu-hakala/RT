92d86f75a9c83be43c7fab9236fd7eb2f51c7b00
Debbie Elliott <rigbydebbie@icloud.com>
2022-11-16 15:55:58 +0200

    added world space w = 0

--- srcs/normals.c
+++ srcs/normals.c
@@ -6,7 +6,7 @@
 /*   By: deelliot <deelliot@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 10:10:20 by deelliot          #+#    #+#             */
-/*   Updated: 2022/11/16 15:52:34 by deelliot         ###   ########.fr       */
+/*   Updated: 2022/11/16 15:55:36 by deelliot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,7 @@ t_tuple	object_to_world_space(t_mtx *inverse, t_tuple obj_space)
 
 	transposed_inverse = transpose_matrix(inverse);
 	world_space = matrix_tuple_multi(&transposed_inverse, &obj_space);
+	world_space.tuple.units.w = 0;
 	return (world_space);
 }
 
