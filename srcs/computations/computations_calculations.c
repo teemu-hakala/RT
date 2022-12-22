39d0e845c8612ab1aa6cf06ef8199bd9c36bfa76
Debbie Elliott <rigbydebbie@icloud.com>
2022-12-22 16:44:03 +0200

    changed computation format

--- srcs/computations/computations_calculations.c
+++ srcs/computations/computations_calculations.c
@@ -36,8 +36,6 @@ void	prepare_object(t_world *world, t_object *object, t_comp *computations, \
 		computations->inside = false;
 	computations->over_point = tuple_add(computations->point, \
 		tuple_scale(computations->vectors.surface_normal, EPSILON));
-	computations->reflectv = reflect(ray.direction, computations->vectors.surface_normal);
-
-
-
+	computations->reflectv = reflect(ray.direction, \
+		computations->vectors.surface_normal);
 }
