73252350b8c85fa6124f6811869aa3dbbb3eea51
Debbie Elliott <rigbydebbie@icloud.com>
2022-12-29 11:46:31 +0200

    merge conflict

--- srcs/intersect/intersect_plane.c
+++ srcs/intersect/intersect_plane.c
@@ -16,16 +16,10 @@ void	plane_intersection(t_ray ray, void *plane, t_world *world)
 {
 	t_intersect	temp;
 
-<<<<<<< HEAD
 	temp.shape = plane;
 	ray = ray_transform(&ray, &((t_sphere *)plane)->transform.inverse);
 	if (EPSILON > ray.direction.tuple.units.y \
 		&& ray.direction.tuple.units.y > -EPSILON)
-=======
-	ray = ray_transform(&ray, &((t_plane *)plane)->transform.inverse);
-	if (PLANE_EPSILON > ray.direction.tuple.units.y \
-		&& ray.direction.tuple.units.y > -PLANE_EPSILON)
->>>>>>> main
 		return ;
 	temp.time = -ray.origin.tuple.units.y / ray.direction.tuple.units.y;
 	if (vec_push(&world->intersections, &temp) == VEC_ERROR)
