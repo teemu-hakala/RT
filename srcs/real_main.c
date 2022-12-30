ce57d2a3994903ab7b52eff86369d3001bc55e46
Debbie Elliott <rigbydebbie@icloud.com>
2022-12-30 10:52:47 +0200

    new pattern structure, with uv mapping for most objects. testing in main.c

---
+++ srcs/real_main.c
@@ -0,0 +1,31 @@
+#include "RT.h"
+
+static void	open_scene_into(t_win *win, const char *str)
+{
+	win->fd = open(str, O_RDONLY);
+	if (win->fd < 0)
+		handle_errors("unable to open file");
+}
+
+int	close_success(void)
+{
+	handle_errors("All done :)");
+	return (0);
+}
+
+// int	main(int argc, char **argv)
+// {
+// 	t_win	win;
+
+// 	if (argc != 2)
+// 		handle_errors(USAGE);
+// 	open_scene_into(&win, argv[1]);
+// 	initialise_world(&win.world);
+// 	parse_into(&win.world, win.fd);
+// 	initialise_window(&win);
+// 	render(&win, &win.world.camera);
+// 	mlx_hook(win.win, KEY_DOWN, 0, handle_input, &win);
+// 	mlx_hook(win.win, MOUSE_ESC, 0, close_success, &win);
+// 	mlx_loop(win.mlx);
+// 	return (0);
+// }
