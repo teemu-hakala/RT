6b720a90696125c073199a7f8a22abba43d56687
Debbie Elliott <deelliot@c1r5p6.hive.fi>
2023-01-12 15:01:50 +0200

    adjusting for cone maping

--- srcs/patterns/uv_mapping.c
+++ srcs/patterns/uv_mapping.c
@@ -40,10 +40,6 @@ t_uv_map	cylindrical_map(t_tuple *p)
 	map.v = fmod(p->tuple.units.y, 1);
 	if (map.v < 0)
 		map.v += 1;
-	if (map.v > 1 || map.v < 0)
-		printf("v = %f\n", map.v);
-	if (map.u > 1 || map.u < 0)
-		printf("u = %f\n", map.u);
 	return (map);
 }
 
@@ -54,7 +50,8 @@ t_uv_map	conical_map(t_tuple *p)
 
 	theta = atan2(p->tuple.units.x, p->tuple.units.z);
 	map.u = 1 - ((theta / (2 * M_PI)) + 0.5);
-	map.v = fmod((2 * p->tuple.units.y + 1), 1); //no idea if this is correct
+	map.v = fmod(p->tuple.units.y, 1);
+	//map.v = fmod((2 * p->tuple.units.y + 1), 1); //no idea if this is correct
 	if (map.v < 0)
 		map.v += 1;
 	if (map.v > 1 || map.v < 0)
