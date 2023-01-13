0f7eec8b86bd3a1a2ec42b8afba816ad416420d1
Teemu Hakala <thakala@c1r5p8.hive.fi>
2023-01-13 12:16:06 +0200

    put_image in correct order

--- srcs/render/put_image.c
+++ srcs/render/put_image.c
@@ -94,10 +94,10 @@ int	put_image(t_win *win)
 		win->drawn = 0;
 	}
 	mlx_put_image_to_window(win->mlx, win->win, win->img.img, 0, 0);
+	put_keys_image(win);
 	if (win->drawn == false)
 		mlx_put_image_to_window(win->mlx, win->win, \
 			progress_bar_image(win, NULL, BAR_NON_ACTION).img, 10, HEIGHT - 30);
-	put_keys_image(win);
 	if (win->drawn == true)
 		mlx_loop_hook(win->mlx, NULL, NULL);
 	return (0);
