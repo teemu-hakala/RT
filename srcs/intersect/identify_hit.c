bfb5c941b3bda40d25591d17441f932ab45262f5
teemu-hakala <78655964+teemu-hakala@users.noreply.github.com>
2023-01-14 19:29:30 +0200

    end of day push

--- srcs/intersect/identify_hit.c
+++ srcs/intersect/identify_hit.c
@@ -60,19 +60,30 @@ void	identify_hit(t_world *world, t_hit *hit)
 	t_vec			positive_front_ids;
 	t_vec			negative_front_ids;
 	t_intersect		*intersection;
+	// uint64_t		positive_misses;
 	uint64_t		i;
+	bool			shadow_mode;
 
+	shadow_mode = false;
+	if (hit == &world->shadow_hit)
+		shadow_mode = true;
+	(void)shadow_mode;
 	positive_front_ids.memory = NULL;
 	negative_front_ids.memory = NULL;
 	initialise_front_vecs(&positive_front_ids, &negative_front_ids);
 	i = 0;
+	// positive_misses = 0;
 	hit->hit_check = false;
 	while (i < world->intersections.len)
 	{
 		intersection = \
 			(t_intersect *)vec_get(&world->intersections, i++);
 		if (is_positive_object(&intersection->shape))
+		{
+			// if (intersection->time < 0)
+			// 	positive_misses++;
 			volume_negator(world, &positive_front_ids, &intersection->shape);
+		}
 		else
 			volume_negator(world, &negative_front_ids, &intersection->shape);
 		if (intersection->time >= 0 && negative_front_ids.len == 0
@@ -82,6 +93,7 @@ void	identify_hit(t_world *world, t_hit *hit)
 			change_shape_material(&hit->intersection.shape, \
 				vec_get(&world->objects, \
 					*(uint64_t *)positive_front_ids.memory));
+					// ((uint64_t *)positive_front_ids.memory)[positive_misses]));
 			hit->hit_check = true;
 			break ;
 		}
