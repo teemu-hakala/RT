a898adba9efd8e2dc501c2f608f044a14d5ec616
Teemu Hakala <thakala@c2r4p8.hive.fi>
2023-01-19 19:13:27 +0200

    perfected out?

--- srcs/cool_interface/movement_agent.c
+++ srcs/cool_interface/movement_agent.c
@@ -26,13 +26,19 @@ void	camera_upwards(t_win *win, t_fl magnitude)
 
 void	camera_forwards(t_win *win, t_fl magnitude)
 {
+	t_tuple	difference;
+
+
 	(void)magnitude;
 	win->input.mouse.mode = CAMERA_FORWARD;
-	win->world.camera.transform.translation.tuple.units.z += magnitude;
-	// win->world.camera.origin.tuple.units.z += magnitude;
-	win->world.camera.center_of_interest.array[Z] += magnitude;
-	// transform_camera(&win->world.camera);
-	transform_camera_for_rotations(&win->world.camera);
+	difference = tuple_sub(win->world.camera.center_of_interest, \
+		win->world.camera.origin);
+	difference = tuple_scale(difference, magnitude);
+	win->world.camera.origin = tuple_add(win->world.camera.origin, \
+		difference);
+	win->world.camera.center_of_interest = \
+		tuple_add(win->world.camera.center_of_interest, difference);
+	transform_camera(&win->world.camera);
 }
 
 void	camera_sideways(t_win *win, t_fl magnitude)
