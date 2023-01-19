50a5f085f17c444f07424cf044a1f495e34a7d6e
Teemu Hakala <thakala@c2r4p8.hive.fi>
2023-01-19 19:57:05 +0200

    attempt to calculate the angles and fix the snapping of the camera

--- srcs/camera/first_person_camera.c
+++ srcs/camera/first_person_camera.c
@@ -11,16 +11,41 @@
 
 
 #include "RT.h"
+ #	include <stdio.h>
 void	print_tuple(t_tuple tuple, const char *name);
 void	rot_x(t_mtx *mtx, t_fl angle);
 void	rot_y(t_mtx *mtx, t_fl angle);
 void	rot_z(t_mtx *mtx, t_fl angle);
+
+t_fl	angle_between_vectors(t_tuple a, t_tuple b)
+{
+	t_fl	angle;
+
+	angle = acos(dot_product(a, b) / magnitude(a) * magnitude(b));
+	printf("angle_between vectors: %lf\n", angle);
+	return (angle);
+}
+
+bool	calculate_initial_angle(t_tuple *rotation, t_tuple *coi, t_tuple *snap)
+{
+	t_tuple		coi_proj_xz;
+
+	coi_proj_xz = vector(coi->array[X], 0, coi->array[Z]);
+	rotation->array[X] = -angle_between_vectors(*coi, coi_proj_xz);
+	rotation->array[Y] = -angle_between_vectors(*snap, coi_proj_xz);
+	return (false);
+}
+
 void	first_person_camera(t_win *win)
 {
 	static t_tuple	rotation;
+	static bool		first = true;
 	t_object		*object;
 	t_mtx			matrix;
 
+	if (first)
+		first = calculate_initial_angle(&rotation, \
+			&win->world.camera.center_of_interest, &(t_tuple){{{0, 0, 1, 1}}});
 	object = ((t_object *)vec_get(&win->world.objects, 0));
 	transform_object(&object->object.sphere.transform);
 	print_tuple(object->object.sphere.transform.translation, \
