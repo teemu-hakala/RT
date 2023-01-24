eec74016a19611bd576870dcea07816ddda25539
Debbie Elliott <rigbydebbie@icloud.com>
2023-01-24 17:05:17 +0200

    fixed leak

--- srcs/computations/compute_refraction.c
+++ srcs/computations/compute_refraction.c
@@ -2,7 +2,7 @@
 
 static void	check_container(t_vec *container, t_intersect *current)
 {
-	int check;
+	int			check;
 	uint64_t	j;
 
 	check = false;
@@ -10,13 +10,13 @@ static void	check_container(t_vec *container, t_intersect *current)
 	while (j < container->len)
 	{
 		if (current->shape->shape_id == \
-			((t_container *)vec_get(container, j++))->shape_id)
+			((t_container *)vec_get(container, j))->shape_id)
 		{
 			if (vec_remove(container, j) != VEC_SUCCESS)
 				handle_errors("unable to remove object from container");
 			check = true;
-			break ;
 		}
+		j++;
 	}
 	if (check == false)
 		if (vec_push(container, current) != VEC_SUCCESS)
@@ -31,7 +31,7 @@ void	compute_refraction_index(t_world *world, t_comp *computation)
 
 	i = 0;
 	vec_new(&container, 1, sizeof(t_container));
-	while (i++ < world->intersections.len)
+	while (i < world->intersections.len)
 	{
 		current = vec_get(&world->intersections, i);
 		if (current->time == world->hit.intersection.time)
@@ -45,12 +45,13 @@ void	compute_refraction_index(t_world *world, t_comp *computation)
 		check_container(&container, current);
 		if (current->time == world->hit.intersection.time)
 		{
-			if (container.len == 1)
+			if (container.len == 0)
 				computation->n2 = 1.0;
 			else
 				computation->n2 = ((t_container *)vec_get(&container, \
 					container.len - 1))->material.refractive_index;
 		}
+		i++;
 	}
 	vec_free (&container);
 }
