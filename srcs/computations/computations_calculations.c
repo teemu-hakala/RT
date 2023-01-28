82bbd46716e452f780dd4d7733a73e1432fd8022
Debbie Elliott <rigbydebbie@icloud.com>
2023-01-28 17:28:49 +0200

    fixed norm in computations

--- srcs/computations/computations_calculations.c
+++ srcs/computations/computations_calculations.c
@@ -15,7 +15,8 @@ static t_tuple	hit_position(t_ray *ray, t_fl distance)
 void	prepare_object(t_world *world, t_object *object, t_ray ray)
 {
 	world->hit.computations.time = world->hit.intersection.time;
-	world->hit.computations.point = hit_position(&ray, world->hit.computations.time);
+	world->hit.computations.point = \
+		hit_position(&ray, world->hit.computations.time);
 	world->hit.computations.vectors.eye = tuple_scale(ray.direction, -1);
 	world->hit.computations.vectors.surface_normal = \
 		normal_at(object, &world->hit.computations.point);
@@ -28,11 +29,13 @@ void	prepare_object(t_world *world, t_object *object, t_ray ray)
 	}
 	else
 		world->hit.computations.inside = false;
-	world->hit.computations.over_point = tuple_add(world->hit.computations.point, \
+	world->hit.computations.over_point = \
+		tuple_add(world->hit.computations.point, \
 		tuple_scale(world->hit.computations.vectors.surface_normal, EPSILON));
 	world->hit.computations.reflectv = reflect(ray.direction, \
 		world->hit.computations.vectors.surface_normal);
 	compute_refraction_index(world);
-	world->hit.computations.under_point = tuple_sub(world->hit.computations.point, \
+	world->hit.computations.under_point = \
+		tuple_sub(world->hit.computations.point, \
 		tuple_scale(world->hit.computations.vectors.surface_normal, EPSILON));
 }
