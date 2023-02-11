9046c49512c68524d552c1d3b38e874dbb3c8cde
Teemu Hakala <thakala@c2r3p11.hive.fi>
2023-02-11 17:14:31 +0200

    fast fixes for progress bar positioning

--- srcs/cool_interface/progress_bar.c
+++ srcs/cool_interface/progress_bar.c
@@ -6,13 +6,11 @@
 /*   By: thakala <thakala@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 11:20:40 by deelliot          #+#    #+#             */
-/*   Updated: 2023/02/11 17:09:00 by thakala          ###   ########.fr       */
+/*   Updated: 2023/02/11 17:13:27 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RT.h"
-
-# 	 include	 	 <stdio.h>
 
 t_img	progress_bar_image(t_win *win, t_canvas *bar_dimensions, uint8_t flag)
 {
@@ -21,7 +19,6 @@ t_img	progress_bar_image(t_win *win, t_canvas *bar_dimensions, uint8_t flag)
 
 	if (bar_img.img == NULL)
 	{
-		printf("bar_img.img == NULL\n");
 		bar_img.dimensions = (t_rectangle){{}, *bar_dimensions, {}};
 		bar_img.img = mlx_new_image(win->mlx, \
 			bar_dimensions->horizontal, bar_dimensions->vertical);
@@ -33,8 +30,6 @@ t_img	progress_bar_image(t_win *win, t_canvas *bar_dimensions, uint8_t flag)
 			handle_errors("bar_img.addr got NULL");
 		filler = (t_rectangle){{}, *bar_dimensions, {}};
 	}
-	printf("t_rectangle filler: start [y, x] [%u, %u], end [y, x] [%u, %u]\n", filler.start.vertical, filler.start.horizontal, filler.end.vertical, filler.end.horizontal);
-	printf("t_rectangle filler: start [y, x] [%u, %u], end [y, x] [%u, %u]\n", filler.start.vertical, filler.start.horizontal, filler.end.vertical, filler.end.horizontal);
 	if (flag & BAR_CLEAR || bar_img.img == NULL)
 		put_rectangle_to_image(bar_img, filler, filler, \
 			(t_tuple){.tuple.colour = {0, 0, 0, 1}});
@@ -45,7 +40,6 @@ static void	rect_pixel_put(t_img img, int y, int x, uint32_t colour)
 {
 	char	*pixel;
 
-	printf("[y, x] === [%i, %i]\n", y, x);
 	pixel = img.addr + (y * img.length + x * (img.bpp / 8));
 	*(uint32_t *)pixel = colour;
 }
@@ -69,21 +63,17 @@ void	put_rectangle_to_image(t_img img, t_rectangle image,
 	uint32_t	colour_hex;
 	t_canvas	point;
 
-	printf("t_rectangle image: start [y, x] [%u, %u], end [y, x] [%u, %u]\n", image.start.vertical, image.start.horizontal, image.end.vertical, image.end.horizontal);
 	colour_hex = clamped_rgba_to_hex(&colour.tuple.colour);
 	fit_rectangle_to_image_bounds(&rect, image);
 	point.vertical = rect.start.vertical;
 	while (point.vertical < rect.end.vertical)
 	{
 		point.horizontal = rect.start.horizontal;
-		printf("rect.end.horizontal: %u\n", rect.end.horizontal);
 		while (point.horizontal < rect.end.horizontal)
 		{
 			rect_pixel_put(img, point.vertical, point.horizontal, colour_hex);
 			point.horizontal++;
 		}
-		if (point.vertical == 19)
-			printf("asd\n");
 		point.vertical++;
 	}
 }
@@ -94,15 +84,14 @@ void	progress_bar(t_img img, t_fl progress_percentage, t_fl prev_perc)
 	t_rectangle	rectangle;
 	t_tuple		colour;
 
-	printf("t_rectangle img.dimensions start [y,x] [%u, %u], end [y,x] [%u, %u]\n", img.dimensions.start.vertical, img.dimensions.start.horizontal, img.dimensions.end.vertical, img.dimensions.end.horizontal);
 	image_bounds = (t_rectangle){.start = {.horizontal = \
-		(img.dimensions.end.horizontal - 20) * \
+		(img.dimensions.end.horizontal) * \
 		prev_perc, .vertical = 0}, \
-		.end = {.horizontal = img.dimensions.end.horizontal - 20, \
+		.end = {.horizontal = img.dimensions.end.horizontal, \
 		.vertical = 20}};
 	rectangle = (t_rectangle){.start = {.horizontal = progress_percentage, \
 		.vertical = 0}, \
-		.end = {.horizontal = (img.dimensions.end.horizontal - 20) \
+		.end = {.horizontal = (img.dimensions.end.horizontal) \
 			* progress_percentage + 10, \
 		.vertical = 20}};
 	if (prev_perc == 0)
